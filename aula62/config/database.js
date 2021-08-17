93394b3e4efc52dd92bfc82f16069aa3eb14417b
DIEGO FELIPE DOS SANTOS <diego.santos3@alunos.sc.senac.br>
2021-08-17 17:06:22 -0300

    aula 63

--- aula62/config/database.js
+++ aula62/config/database.js
@@ -3,6 +3,6 @@ module.exports = {
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-    // logging: false
+    database: process.env.DB_DATABASE,
+    logging: false
 };
